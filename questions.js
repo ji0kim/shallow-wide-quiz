2619f29ee5b3ec4e56393538d2e649d21ed3ed26
Jay <jiyoung26091212@gmail.com>
2022-04-04 13:47:13 +0100

    Add questions

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+	{
+		question: '1- Which of the following retains the information it’s storing when the power to the system is turned off?',
+		options: ['CPU', 'ROM', 'RAM', 'DIMM'],
+		answer: 'ROM',
+		isUsed: false,
+	},
+	{
+		question: '2-A network of computers and other devices that is confined to a relatively small space is called?',
+		options: [' Wide Area Network', 'Local Area Network', 'Global Network', 'Peer-to-Peer Network'],
+		answer: 'Local Area Network',
+		isUsed: false,
+	},
+	{
+		question: '3-Every computer connected to the Internet is identified by a unique four-part string, known as',
+		options: ['IP address', 'Host name', 'Domain name', 'None of the above'],
+		answer: 'IP address',
+		isUsed: false,
+	},
+];
